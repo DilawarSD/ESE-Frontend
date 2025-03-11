8d4cce33a85776b4a3d1a920ac6460ac3d747249
Dilawar Dayal <dilawardayal@Dilawars-MacBook-Pro.local>
2025-03-11 11:11:15 +0000

    start up for frontend

---
+++ ese-frontend/src/app/taskmanagement/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import App from "../components/App";
+
+export default function CreatePostPage() {
+  return (
+    <div>
+      <main>
+        <App />
+      </main>
+    </div>
+  );
+}
